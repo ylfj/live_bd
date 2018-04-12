0ff1d834cfdd917de47c9e7f1adde1ce23ae9891
klren0312 <kuanglong0312@sina.com>
2018-04-12 17:33:47 +0800

    接口

--- router.js
+++ router.js
@@ -1,9 +1,11 @@
 const router = require('koa-router')()
 const UserController = require('./controller/user')
 module.exports = (app) => {
-  router.post('/register', UserController.register)
-  router.post('/login', UserController.login)
-  router.get('/userinfo', UserController.userinfo)
+  router.post('/login', UserController.login)            // 登录
+  router.post('/register', UserController.register)      // 注册
+  router.get('/userinfo', UserController.getUserinfo)    // 获取用户信息
+  router.del('/userinfo/:id',UserController.delUserinfo) // 删除用户
+  router.put('/userinfo', UserController.putUserInfo)    // 更新用户信息
   app.use(router.routes())
     .use(router.allowedMethods())
 }

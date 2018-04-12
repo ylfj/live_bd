ccec0f3159dabe2b12c1544240d64691b9281e64
klren0312 <kuanglong0312@sina.com>
2018-04-12 17:34:23 +0800

    日志

--- middleware/mi-log/logger.js
+++ middleware/mi-log/logger.js
@@ -1,62 +1,21 @@
 const log4js = require('log4js');
-// 引入日志输出信息的封装文件
-const access = require("./access.js");
-const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
-
-const baseInfo = {
-  appLogLevel: 'debug',
-  dir: 'logs',
-  env: 'dev',
-  projectName: 'koa2-tutorial',
-  serverIp: '0.0.0.0'
-}
-const { env, appLogLevel, dir, serverIp, projectName } = baseInfo
-// 增加常量，用来存储公用的日志信息
-const commonInfo = { projectName, serverIp }
-module.exports = () => {
-  const contextLogger = {}
-  const appenders = {}
-
-  appenders.cheese = {
-    type: 'dateFile',
-    filename: `${dir}/task`,
-    pattern: '-yyyy-MM-dd.log',
-    alwaysIncludePattern: true
-  }
-  
-  if (env === "dev" || env === "local" || env === "development") {
-    appenders.out = {
-      type: "console"
-    }
-  }
-  let config = {
-    appenders,
-    categories: {
-      default: {
-        appenders: Object.keys(appenders),
-        level: appLogLevel
-      }
-    }
-  }
-
-  const logger = log4js.getLogger('cheese');
-
+module.exports = ( options ) => {
   return async (ctx, next) => {
     const start = Date.now()
-
-    log4js.configure(config)
-    methods.forEach((method, i) => {
-      contextLogger[method] = (message) => {
-       // 将入参换为函数返回的字符串
-        logger[method](access(ctx, message, commonInfo))
-      }
-    })
-    ctx.log = contextLogger;
-
+    log4js.configure({
+      appenders: { 
+        BaogoHi: { 
+          type: 'dateFile',
+          filename: `logs/task`,
+          pattern: '-yyyy-MM-dd.log',
+          alwaysIncludePattern: true 
+      }},
+      categories: { default: { appenders: ['BaogoHi'], level: 'info' } }
+    }); 
+    const logger = log4js.getLogger('BaogoHi');
     await next()
-    const responseTime = Date.now() - start;
-    logger.info(access(ctx, {
-      responseTime: `响应时间为${responseTime/1000}s`
-    }, commonInfo))
+    const end = Date.now()
+    const responseTime = end - start;
+    logger.info(`响应时间为${responseTime/1000}s`);
   }
 }

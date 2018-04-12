4353948f0b37c62a3442f2c5c67b0a006fd9ca31
klren0312 <kuanglong0312@sina.com>
2018-04-12 17:34:07 +0800

    添加jwt

--- middleware/index.js
+++ middleware/index.js
@@ -1,22 +1,19 @@
 const path = require('path')
 const bodyParser = require('koa-bodyparser')
-const nunjucks = require('koa-nunjucks-2')
 const staticFiles = require('koa-static')
+//jwt相关
+const jwtKoa = require('koa-jwt')
+const secret = require('../config/secret.json')
 
 const miSend = require('./mi-send')
 const miLog = require('./mi-log')
 module.exports = (app) => {
-  app.use(staticFiles(path.resolve(__dirname, "../public")))
-
-  app.use(nunjucks({
-    ext: 'html',
-    path: path.join(__dirname, '../views'),
-    nunjucksConfig: {
-      trimBlocks: true
-    }
-  }));
-
-  app.use(bodyParser())
-  app.use(miSend())
-  app.use(miLog())
+  app
+    .use(jwtKoa({secret:secret.sign}).unless({
+      path: ['/login','/register'] //数组中的路径不用通过认证
+    }))
+    .use(staticFiles(path.resolve(__dirname, "../public")))
+    .use(bodyParser())
+    .use(miSend())
+    .use(miLog())
 }
